8e0e007045b499b1a499a21296d58c146ca04688
Joshua <40162974+Joshhook69@users.noreply.github.com>
2019-09-18 13:28:13 -0500

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'crew_base' => ['required', 'string', 'min:3|max:9'],
-	    'vatsim_cid' => ['required', 'string', 'min:6|max:9'],
+            'vatsim_cid' => ['required', 'string', 'min:6|max:9'],
         ]);
     }
 
@@ -70,7 +70,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'crew_base' => $data['crew_base'],
-	    'vatsim_cid' => $data['vatsim_cid']
+            'vatsim_cid' => $data['vatsim_cid']
         ]);
     }
 }
