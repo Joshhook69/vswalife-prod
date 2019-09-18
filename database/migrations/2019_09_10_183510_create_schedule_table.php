09fe3fae0bf918234864e13f8f49487401b800d9
Joshua <40162974+Joshhook69@users.noreply.github.com>
2019-09-18 13:43:15 -0500

    lined it up

--- database/migrations/2019_09_10_183510_create_schedule_table.php
+++ database/migrations/2019_09_10_183510_create_schedule_table.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateScheduleTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+   
     public function up()
     {
         Schema::create('schedule', function (Blueprint $table) {
