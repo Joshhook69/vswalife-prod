8c74a465f9eecf39bed209bc3b128dc824446bee
Joshua <40162974+Joshhook69@users.noreply.github.com>
2019-09-20 13:27:13 -0500

    cleaning up

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,5 @@
 @extends('errors.master')
-<div class="container">
+<div class="container" style="margin-top:10%;>
 	<body>
 		<div class="alert alert-danger" role="alert">
   500 - Internal Server Error
