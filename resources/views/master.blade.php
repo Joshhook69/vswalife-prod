5656a4f2b2fdc655846b14ab8dbe889b3cc360fd
Joshua <40162974+Joshhook69@users.noreply.github.com>
2019-09-20 13:39:50 -0500

    need to actually clean up

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,7 +7,6 @@
 	<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>-->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	<script src="{{ asset('js/app.js') }}" defer></script>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
