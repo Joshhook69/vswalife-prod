05a3ccf4bca898d4ece92add5fea0431aea78ad0
Joshua <40162974+Joshhook69@users.noreply.github.com>
2019-09-20 13:32:45 -0500

    cleaning up

--- resources/views/site/commandments.blade.php
+++ resources/views/site/commandments.blade.php
@@ -3,26 +3,15 @@
 <body>
 	<div class="container" style="margin-top:10%;">
 	<h1>The 10 ATC vectoring Commandments given to <br>Moses from the Administrator of the FAA:</h1>
-
 <p class="lead">1. Thou shalt not allow an aircraft to crash into another aircraft willingly.</p>
-
 <p class="lead">2. Thou shalt not allow an aircraft to crash into terrain or man-made obstructions willingly.</p>
-
 <p class="lead">3. Thou shalt not vector an aircraft to join the final approach course within the approach gate. If the aircraft was cleared on an instrument approach and appears to be off course inside the approach gate, the controller shalt advise the pilot of his/her position and distance from the airport and right/left of centerline and to ADVISE HIS/HER INTENTIONS. <ins>DO NOT VECTOR HIM/HER BACK TOWARDS THE LOCALIZER.</ins> </p>
-
 <p class="lead">4. Thou shalt not give an aircraft more than a 20 degree intercept when vectoring it to join the final 2 miles or less from the approach gate, as well as if TRIPS are in use (Aircraft shalt be established and switched to tower prior to the TRIPS capture box). </p>
-
 <p class="lead">5. Thou shalt not give an aircraft more than a 30 degree intercept heading to join the final approach course when joining at least more than 2 miles from the approach gate (45 degrees shalt be used for rotorcraft in lieu of the 30 degree max rule). </p>
-
 <p class="lead">6. Thou shalt give a reason for vector to the pilot when vectoring his/her aircraft off course. </p>
-
 <p class="lead">7. Thou shalt not vector an aircraft that is below the Minimum Vectoring Altitude (MVA) unless the aircraft is within a Diverse Vector Area (DVA) and/or the controller has asked the pilot if he/she is able to maintain his/her own terrain and obstruction avoidance through (the MVA) and the pilot has confirmed that they are able to do so. </p>
-
 <p class="lead">8. Thou shalt always give a pilot an altitude to maintain when vectoring off course from a SID/STAR or when the aircraft is not in straight and level flight. As well, Thou shalt always ensure that all IFR aircraft landing at a bravo airport shalt remain inside the Bravo at all times on approach to the airport to the highest extent possible. If an IFR arrival leaves the Bravo during his approach, the controller shalt notify the pilot that he/she is leaving bravo airspace and can expect to re-enter the bravo in X amount of miles/minutes (workload permitting) </p>
-
-
 <p class="lead">9. Thou shalt always abide by minimum terminal (3NM lat/long - 1000ft Vert [IFR] ::: lat/long/vert - See .65 ... way too complicated to explain in a bible verse [VFR] ) / enroute (5NM lat/long - 1000ft Vert [IFR] ::: lat/long/vert - target resolution [VFR]) radar separation minima for IFR/VFR targets and when applicable, apply wake turbulence separation. <ins>NOTE: Visual separation SHALT NOT be used in lieu of radar/wake turbulence separation when a SUPER is the leading aircraft.</ins> </p>
-
 <p class="lead">10. Thou shalt always give a pilot that has the field in sight a 30 degree intercept to join the final prior to clearing him/her for a visual approach when SIMULs are in use and/or to an airport with at least two parallel runways. </p>
 </div>
 </body>
