9f367325e137cac5e7160d69cdfd1b6efb0a3fa6
Joshua <40162974+Joshhook69@users.noreply.github.com>
2019-09-20 13:23:07 -0500

    cleaning up
    it was @extending layouts.app which isn't a thing.

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,6 +1,5 @@
-@extends('layouts.app')
-
-<div class="container">
+@extends(master)
+<div class="container" style="margin-top:10%">
 	<body>
 		<div class="alert alert-danger" role="alert">
   403 - Permission Denied
