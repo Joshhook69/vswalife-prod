de46750fc47bcc7ce02680b14fa348eefdc162d3
Joshua <40162974+Joshhook69@users.noreply.github.com>
2019-10-01 09:38:53 -0500

    Update BookingController.php

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Booking;
 
 class BookingController extends Controller
 {
     public function index() {
+        if(Auth::user()){
 	return view('booking.index');
+    }
     }
 }
