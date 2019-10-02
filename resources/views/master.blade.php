011c2c2ff6cd0142f685bdaaedfc019a7496cb1d
root <root@hooker.services>
2019-10-02 18:16:39 +0000

    Home Page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,10 +3,10 @@
 	<title>
 	    vSWALife - @yield('title')
 	</title>
-	<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>-->
-	<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/assets/css/bookbutton.css') }}" rel="stylesheet">
 	<script src="{{ asset('js/app.js') }}" defer></script>
