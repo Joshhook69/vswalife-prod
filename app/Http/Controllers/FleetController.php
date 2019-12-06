9b9956916830e8ca84586daaeabd50042cf03ba7
root <root@hooker.services>
2019-12-06 23:37:20 +0000

    merge

--- app/Http/Controllers/FleetController.php
+++ app/Http/Controllers/FleetController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Respose;
 use App\Fleet;
 
 class FleetController extends Controller
@@ -13,7 +14,7 @@ class FleetController extends Controller
     	$fleet = Fleet::paginate(30);
     	return view('site.fleet')->with('fleet', $fleet);
     }else{
-    	return redirect('/')->withErrors('msg', 'Not Authorized');
+    	return redirect('/')->withErrors('message', 'Not Authorized');
     }
 }
 }
